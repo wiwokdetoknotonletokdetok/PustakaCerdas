0cb782eed827aee1db0c1b103b0f71e003fce294
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-07-15 19:36:41 +0700

    [GREEN] Set default value

--- app/controller/book.py
+++ app/controller/book.py
@@ -7,6 +7,6 @@ router = APIRouter()
 
 
 @router.get("/books")
-def get_books(q: str = Query(None), max: int = Query(None), threshold: float = Query(None)):
+def get_books(q: str = Query(None), max: int = Query(5), threshold: float = Query(0.40)):
     response = WebResponse.builder().data(search_book(q, max, threshold)).build()
     return response.dict()
--- app/services/book_service.py
+++ app/services/book_service.py
@@ -16,7 +16,7 @@ def save_book(message: AmqpBookMessage):
     )
 
 
-def search_book(query: str, top_k: int = 5, score_threshold: float = 0.70) -> list[BookPayload]:
+def search_book(query: str, top_k: int, score_threshold: float) -> list[BookPayload]:
     query_vector = get_embedding(query)
 
     search_result = qdrant.search(
