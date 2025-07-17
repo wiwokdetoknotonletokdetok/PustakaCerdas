dc09e3e054aa82457c6ae31424fb868cc18fac32
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-07-17 19:55:07 +0700

    [BUILD] Add allow origin

--- app/main.py
+++ app/main.py
@@ -3,8 +3,17 @@ from fastapi import FastAPI
 from app.config import setup_database
 from app.controller import book_router
 from app.consumer import setup_consumer
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # atau ["*"] untuk semua origin (testing)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(book_router)
 
